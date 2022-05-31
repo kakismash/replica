f7c0e51ab9b3e9d1e61024b8ccaaff1a56de7a83
Joaquin Navarro <alfian1991@gmail.com>
2022-05-31 19:14:23 -0400

    ionic chat xample

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketService as SocketService } from '../service/socket.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  status = false;
+  messages: Array<string> = [];
+
+  constructor(private socketService: SocketService) {}
+
+  ngOnInit(): void {
+    this.openConnection();
+  }
+
+  protected openConnection(): void {
+    this.socketService.open();
+    this.status = true;
+    this.listen();
+  }
+
+  protected listen(): void {
+    this.socketService
+        .listen()
+        .subscribe(m => {
+          this.messages.push(m);
+        });
+  }
+
+  protected sendMessage(message: string): void {
+    this.socketService.sendMessage(message);
+  }
 
 }
---
+++ src/app/service/socket.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { env } from 'process';
+import { Observable } from 'rxjs';
+import * as SocketIOServer from 'socket.io';
+import * as SocketIOClient from 'socket.io-client';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socketClient: SocketIOClient.Socket;
+  constructor() {}
+
+  open(): void {
+    this.socketClient = SocketIOClient.io(environment.wsUrl);
+  }
+
+  listen(): Observable<any> {
+    return new Observable(o => {
+      this.socketClient.on('message', (data) => {
+        console.log('Received message from Websocket Server');
+        o.next(data);
+      });
+    });
+  }
+
+  sendMessage(message: string): void {
+    this.socketClient.emit('message', message);
+  }
+
+}
